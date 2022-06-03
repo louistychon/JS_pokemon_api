c8268bc09f5eaf83808e2da207ece684702395b0
louis.tychon1@gmail.com <louis.tychon1@gmail.com>
2022-06-03 16:58:07 +0200

    images added

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,15 @@ function getPokemon(name) {
         document.write(" " + data.id)
         document.write(" attaque 1 " + data.abilities[0].ability.name)
         document.write("<br>");
+        document.write("<br>");
+        document.write("<img src=" + data.sprites.front_default + ">")
+        document.write("<br>");
     }).catch((err) => {
         console.log("pokemon not found", err)
     })
 }
-
 //loop that collects all pokemons
-for (let index = 1; index < 351; index++) {
+for (let index = 0; index < 351; index++) {
     getPokemon(index)
 }
+console.log("pokedex récupéré")
